768f70390634c3453ce02776191bc0eb9adbef83
Nathnael-G <nathnaelgetachew12f@gmail.com>
2025-11-12 15:56:15 +0300

    See basic routing, and general styling in tailwind config

--- app/index.tsx
+++ app/index.tsx
@@ -5,8 +5,21 @@ export default function Index() {
     <View
 className="flex-1 justify-center items-center"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text className="text-red-500">This is my movie</Text>
+      <Text className="text-red-500">This is my movie App</Text>
+            <Text className="text-dark-200 text-5xl font-bold">Welcome</Text>
+            {/* <Link href="/onboarding" className="mt-4 px-4 py-2 bg-blue-500 rounded text-white">
+        Go to Onboarding
+      </Link>
+<Link
+  href={{
+    pathname: "/movie/[id]",
+    params: { id: "avengers" }
+  }}
+  className="mt-4 px-4 py-2 bg-accent rounded text-white"
+>
+  movie details
+</Link> */}
+
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#030014',
+        secndary: '#151312',
+        accent: '#ab8bff',
+        light:{
+          100: '#D6C6FF',
+          200: '#A8B5DB',
+          300: '#9C4AAB',
+        },
+        dark:{
+          100: '#221f3D',
+          200: '#0f0d23',
+ },
+        background: '#F5F5F5',
+        textPrimary: '#333333',
+        textSecondary: '#666666',
+      },
+    },
   },
   plugins: [],
 }
